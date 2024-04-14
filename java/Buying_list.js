40a82cbaac324d8c5e65bd21728de9e0abddf592
Simon-ict <109569199+Simon-ict@users.noreply.github.com>
2024-04-14 21:49:13 +0700

    Add files via upload

--- java/Buying_list.js
+++ java/Buying_list.js
@@ -1,26 +1,31 @@
 function load5img()
 {
-    fetch('http://localhost:8126/Buying_list').then(response => response.json()).then(data => {
+ 
+    const para = new URLSearchParams(window.location.search);
+    const epara = para.get('type');
+    console.log(epara);
+
+    fetch(`http://localhost:8126/Buying_list/${epara}`).then(response => response.json()).then(data => {
 
         console.log(data);
         let listproduct = document.querySelector('.listproduct');
 
-        // var list
-        for(let i = 0 ; i < data.length ; i++){
+        for(let i = 0 ; i < data.length ; i++){    
             
-            listproduct.innerHTML += "<div class='item'>" +
-                                        "<div class='forimg'>" +
-                                            "<img src="+ data[i].picture1 +">" +
-                                        "</div>" +
-                                        "<div class='overlay-image'>" +
-                                            "<img src='../pic/pog_star5.png' alt='Poggo House'>" +
-                                        "</div>" +
-                                        "<div id='name'>" + data[i].p_name + "</div>" +
-                                        "<div id='price'>$" + data[i].p_price + "</div>" +
-                                        "<a href='/House_Detail'><button type='button'>Detail</button></a>" + 
-                                        "<br>" +
-                                    "</div>";
-            }
+                listproduct.innerHTML += "<div class='item'>" +
+                "<div class='forimg'>" +
+                "<img src="+ data[i].picture1 +">" +
+                "</div>" +
+                "<div class='overlay-image'>" +
+                "<img src='../pic/pog_star5.png' alt='Poggo House'>" +
+                "</div>" +
+                "<div id='name'>" + data[i].p_name + "</div>" +
+                "<div id='price'>$" + data[i].p_price + "</div>" +
+                "<a href='/House_Detail/"+ i +"'><button type='button'>Detail</button></a>" + 
+                "<br>" +
+                "</div>";
+          
+        }
 
     })
       .catch(error => console.error('Error fetching images:', error));
