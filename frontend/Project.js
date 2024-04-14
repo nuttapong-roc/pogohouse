998f6e004601818a3c863dd9a1e73715409dbd85
nuttapong-roc <109577863+nuttapong-roc@users.noreply.github.com>
2024-04-14 21:51:44 +0700

    Add files via upload

--- frontend/Project.js
+++ frontend/Project.js
@@ -3,24 +3,24 @@ const path = require('path');
 
 const dotenv = require("dotenv");
 
+const cp = require("cookie-parser");
 const app = express();
 const router = express.Router();
 
-// const port = 8026
-
 dotenv.config();
 
-// import cookie
-const cp = require("cookie-parser");
-router.use(cp());
-
 app.use("/", router);
+
+// router.get('/', (req, res) => {
+  //   res.send("Hello World! in plain text");
+  // })
+router.use(cp());
 
 router.use(express.json());
 router.use(express.urlencoded({extended:true}));
 
 // Read folder as a static file
-router.use(express.static(path.join(__dirname, '/../..', 'pogohouse-main')));
+router.use(express.static(path.join(__dirname, '/../..', 'web programing')));
 
 
 router.get('/', (req, res) => {
@@ -53,26 +53,17 @@ router.get('/search', (req, res) => {
   console.log(`Request at ${__dirname} \nRetrieve a search page`)
 });
 
+router.get('/Buying_listvs', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'html', 'Buying_Detail.html'));
+  console.log(`Request at ${__dirname} \nRetrieve a Buying_list page`)
+});
+
+
+
+// post
 router.get('/login', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'html', 'login.html'));
   console.log(`Request at ${__dirname} \nRetrieve a login page`)
-});
-
-// post
-router.post('/login', function(req, res) {
-
-  var user = req.body.username
-  var password = req.body.password
-
-  console.log(user)
-  console.log(password)
-  if(user == 'booktepbook' && password == '123456'){ 
-    // res.cookie('cookie#1','user' , {expire: 360000 + Date.now()});       <----------- waiting for admin page ---------
-    // res.cookie('cookie#2','Password' , {expire: 360000 + Date.now()});
-    // res.redirect('/admin')
-  }else{
-    res.redirect('/home')
-  }
 });
 
 router.get('/register', (req, res) => {
@@ -80,27 +71,14 @@ router.get('/register', (req, res) => {
   console.log(`Request at ${__dirname} \nRetrieve a register page`)
 });
 
-///////////////////////////////////////////////// Only Admin can access //////////////////////////////////////////////
-// router.get('/admin', (req, res) => {
-// const cookie1 = req.cookies['cookie#1'];
-// const cookie2 = req.cookies['cookie#2']
-//   if(cookie1 && cookie2){
-//     res.sendFile(path.join(__dirname, '..', 'html', 'admin.html'));
-//     console.log(`Request at ${__dirname} \nRetrieve a register page`)
-//   }else{
-//      res.redirect('/home')
-// }
-// });
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-router.use((req, res, next) => {
-  console.log("404: Invalid accessed");
-  // res.status(404).sendFile(path.join(`${__dirname}/error.html`));
-  res.send("404 ERROR")
-})
+// router.use((req, res, next) => {
+//   res.status(404).sendFile(path.join(`${__dirname}/error.html`));
+// })
 
 // Start the server
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on http://localhost:${process.env.PORT}`);
+const port = 8026;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
 
