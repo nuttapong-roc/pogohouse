0ec1663f4500f1e26e9540279a655d38ea3ec937
Simon-ict <109569199+Simon-ict@users.noreply.github.com>
2024-04-14 21:50:16 +0700

    Add files via upload

--- backend/backend.js
+++ backend/backend.js
@@ -6,6 +6,8 @@ const cors = require('cors');
 dotenv.config();
 
 const app = express();
+const router = express.Router();
+
 let corsoption = {
     origin: 'http://localhost:8026',
     methods: 'GET,POST,PUT,DELETE'
@@ -20,25 +22,40 @@ var dbconnect = sql.createConnection({
 });
 
 app.get('/',cors(), (req,res) => {
-    dbconnect.query(`select picture1 from Product order by rand() limit 5 `, function(err, results) {
+    dbconnect.query(`select picture1 from Product order by rand()`, function(err, results) {
         if (err) throw err;
         return res.json(results);
     });
 });
+
+// Buying_list
+
+app.get('/Buying_list',cors(), (req,res) => {
+      dbconnect.query(`select p_name, p_price, categories, picture1 from Product`, function(err, results) {
+        if (err) throw err;
+        return res.json(results);
+    });
+});
+
+app.get('/Buying_list/:id', function (req, res) {
+
+    let type = req.params.id;
+    
+    if (!type) {
+    return res.status(400).send({ error: true, message: 'Please provide student id.' });
+    }
+    dbconnect.query('SELECT p_name, p_price, picture1 FROM Product where categories=?', type, function (error, results) {
+    if (error) throw error;
+    return res.json(results);
+    });
+});
+
 app.get('/homefilp',cors(), (req,res) => {
     dbconnect.query(`select p_name, p_price,p_type,picture1 from Product order by rand() limit 5 `, function(err, results) {
         if (err) throw err;
         return res.json(results);
     });
 });
-app.get('/Buying_list',cors(), (req,res) => {
-    dbconnect.query(`select p_name, p_price, picture1 from Product`, function(err, results) {
-        if (err) throw err;
-        return res.json(results);
-    });
-});
-
-
 
 dbconnect.connect(function(err)
 {
