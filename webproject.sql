a987eccfa3321cacabb18752d25be995b442e6d5
nuttapong-roc <109577863+nuttapong-roc@users.noreply.github.com>
2024-04-15 23:07:11 +0700

    Add files via upload

--- webproject.sql
+++ webproject.sql
@@ -84,7 +84,8 @@ create table Product (
     p_type char(2),
     p_price decimal(13,2),
 	p_description varchar(1500),
-	p_location varchar(30),
+	p_city varchar(30),
+    p_country varchar(30),
 	adminid varchar(10),
 	categories varchar(1),
     picture1 VARCHAR(255),
@@ -96,80 +97,80 @@ create table Product (
 );
 
 
-INSERT INTO Product (p_id, p_name, p_type, p_price, p_description, p_location, adminid, categories, picture1, picture2, picture3, picture4, picture5)
+INSERT INTO Product
 VALUES	('PH_item_001', 'Cat house', 'T', 100000000.00, 
 		'Great house with the purple roof, where the rooms inside the house have something quite mysterious about them. The size of each room always differs every time, but it still has a massive size.', 
-		'Chaville, France', 'AMD001', 'R',
+		'Berlin','Germany', 'AMD001', 'R',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_001/PH_item_001_1.png?raw=true',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_001/PH_item_001_2.jpg?raw=true',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_001/PH_item_001_3.jpg?raw=true',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_001/PH_item_001_4.jpg?raw=true',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_001/PH_item_001_5.jpg?raw=true');
 
-INSERT INTO Product (p_id, p_name, p_type, p_price, p_description, p_location, adminid, categories, picture1, picture2, picture3, picture4, picture5)
+INSERT INTO Product
 VALUES	('PH_item_002', 'Pineapple house undersea', 'H', 3.00, 
 		'The normal size of pineapple has many pieces of furniture inside of it, which it places in the sea.', 
-		'Bikini Bottom, Pacific Ocean', 'AMD001', 'B',
+		'Pattaya','Thailand', 'AMD001', 'B',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_002/PH_item_002_1.jpg?raw=true',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_002/PH_item_002_2.jpg?raw=true',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_002/PH_item_002_3.jpg?raw=true',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_002/PH_item_002_4.jpg?raw=true',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_002/PH_item_002_5.jpg?raw=true');
         
-INSERT INTO Product (p_id, p_name, p_type, p_price, p_description, p_location, adminid, categories, picture1, picture2, picture3, picture4, picture5)
+INSERT INTO Product
 VALUES	('PH_item_003', 'TownHouse Mr.G', 'T', 2500.00, 
 		'Townhouse Japanese Edo style, which has three small rooms inside and looks pretty old.', 
-		'Kanazawa, Japan', 'AMD002', 'R',
+		'Edo', 'Japan', 'AMD002', 'R',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_003/PH_item_003_1.jpg?raw=true',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_003/PH_item_003_2.jpg?raw=true',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_003/PH_item_003_3.jpg?raw=true',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_003/PH_item_003_4.jpg?raw=true',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_003/PH_item_003_5.png?raw=true');
 		
-INSERT INTO Product (p_id, p_name, p_type, p_price, p_description, p_location, adminid, categories, picture1, picture2, picture3, picture4, picture5)
+INSERT INTO Product
 VALUES	('PH_item_004', 'A tower', 'C', 30000000.00, 
 		'A tower is the condo that includes high technology in each room, and it has very strong security guards that are able to protect the universe.', 
-		'Newyork, USA', 'AMD001', 'R',
+		'Newyork','USA', 'AMD001', 'R',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_004/PH_item_004_1.jpg?raw=true',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_004/PH_item_004_2.jpg?raw=true',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_004/PH_item_004_3.jpg?raw=true',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_004/PH_item_004_4.jpg?raw=true',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_004/PH_item_004_5.jpg?raw=true');
 		
-INSERT INTO Product (p_id, p_name, p_type, p_price, p_description, p_location, adminid, categories, picture1, picture2, picture3, picture4, picture5)
+INSERT INTO Product
 VALUES ('PH_item_005', 'Normal house', 'H', 400000.00, 
         'Normal house with modern Japanese style.', 
-        'Tokyo, Japan', 'AMD003', 'B',
+        'Saitama','Japan', 'AMD003', 'B',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_005/PH_item_005_1.jpg?raw=true',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_005/PH_item_005_2.jpg?raw=true',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_005/PH_item_005_3.png?raw=true',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_005/PH_item_005_4.jpg?raw=true',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_005/PH_item_005_5.jpg?raw=true');
 
-INSERT INTO Product (p_id, p_name, p_type, p_price, p_description, p_location, adminid, categories, picture1, picture2, picture3, picture4, picture5)
+INSERT INTO Product
 VALUES ('PH_item_006', 'Normal house 2', 'H', 450000.00, 
         'Normal house with modern Japanese style that is bigger.', 
-        'Tokyo, Japan', 'AMD003', 'B',
+        'Tokyo' ,'Japan', 'AMD003', 'B',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_006/PH_item_006_1.jpg?raw=true',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_006/PH_item_006_2.jpg?raw=true',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_006/PH_item_006_3.jpg?raw=true',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_006/PH_item_006_4.jpg?raw=true',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_006/PH_item_006_5.jpg?raw=true');
 
-INSERT INTO Product (p_id, p_name, p_type, p_price, p_description, p_location, adminid, categories, picture1, picture2, picture3, picture4, picture5)
+INSERT INTO Product
 VALUES ('PH_item_007', 'Unbelievable house', 'H', 30000000.00, 
-        'House in a mystery place where no one knows where it is.', 
-        'Unknown', 'AMD001', 'B',
+        'House in a mystery place where no one where it places, yet we knew it places in Ohio.', 
+        'Ohio','USA', 'AMD001', 'B',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_007/PH_item_007_1.jpg?raw=true',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_007/PH_item_007_2.png?raw=true',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_007/PH_item_007_3.png?raw=true',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_007/PH_item_007_4.jpg?raw=true',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_007/PH_item_007_5.jpg?raw=true');
 		
-INSERT INTO Product (p_id, p_name, p_type, p_price, p_description, p_location, adminid, categories, picture1, picture2, picture3, picture4, picture5)
+INSERT INTO Product
 VALUES ('PH_item_008', 'Chef House', 'H', 1500000.00, 
         'The house that the chef uses to cook his food to run his restaurant, which has a lot of food inside the kitchen.', 
-        'Paris, France', 'AMD003', 'B',
+        'Paris' ,'France', 'AMD003', 'B',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_008/PH_item_008_1.jpg?raw=true',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_008/PH_item_008_2.jpg?raw=true',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_008/PH_item_008_3.jpg?raw=true',
@@ -177,10 +178,10 @@ VALUES ('PH_item_008', 'Chef House', 'H', 1500000.00,
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_008/PH_item_008_5.png?raw=true'
 );
 
-INSERT INTO Product (p_id, p_name, p_type, p_price, p_description, p_location, adminid, categories, picture1, picture2, picture3, picture4, picture5)
+INSERT INTO Product
 VALUES ('PH_item_009', 'House After the End of Earth', 'T', 50000000.00, 
         'The house that has everything humans need to use after the end of the earth is coming.', 
-        'California, USA', 'AMD004', 'B',
+        'California','USA', 'AMD004', 'B',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_009/PH_item_009_1.jpg?raw=true',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_009/PH_item_009_2.jpg?raw=true',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_009/PH_item_009_3.jpg?raw=true',
@@ -188,10 +189,10 @@ VALUES ('PH_item_009', 'House After the End of Earth', 'T', 50000000.00,
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_009/PH_item_009_5.jpg?raw=true'
 );
 
-INSERT INTO Product (p_id, p_name, p_type, p_price, p_description, p_location, adminid, categories, picture1, picture2, picture3, picture4, picture5)
+INSERT INTO Product
 VALUES ('PH_item_010', 'Tree House', 'H', 12000000.00, 
         'The house that was built inside the tree looks like the Yggdrasil that has many pieces of furniture inside.', 
-        'Candy Kingdom, Land of Ooo', 'AMD002', 'B',
+        'Texas','USA', 'AMD002', 'B',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_010/PH_item_010_1.jpg?raw=true',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_010/PH_item_010_2.jpg?raw=true',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_010/PH_item_010_3.jpg?raw=true',
@@ -199,10 +200,10 @@ VALUES ('PH_item_010', 'Tree House', 'H', 12000000.00,
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_010/PH_item_010_5.jpg?raw=true'
 );
 
-INSERT INTO Product (p_id, p_name, p_type, p_price, p_description, p_location, adminid, categories, picture1, picture2, picture3, picture4, picture5)
+INSERT INTO Product
 VALUES ('PH_item_011', 'Plant Save House', 'T', 3000000.00, 
         'The house that has a big garden to plant various types of plants to protect yourself from that "thing"', 
-        'Neighborville, USA', 'AMD002', 'R',
+        'Arizona' ,'USA', 'AMD002', 'R',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_011/PH_item_011_1.jpg?raw=true',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_011/PH_item_011_2.jpg?raw=true',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_011/PH_item_011_3.png?raw=true',
@@ -210,10 +211,10 @@ VALUES ('PH_item_011', 'Plant Save House', 'T', 3000000.00,
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_011/PH_item_011_5.jpg?raw=true'
 );
 
-INSERT INTO Product (p_id, p_name, p_type, p_price, p_description, p_location, adminid, categories, picture1, picture2, picture3, picture4, picture5)
+INSERT INTO Product
 VALUES ('PH_item_012', 'Alien from Krr Planet House', 'T', 25000000.00, 
         'The house has aliens from the Krr planet living inside, and they build secret rooms inside the house that use high technology from other planets.', 
-        'Moscow, Russia', 'AMD001', 'B',
+        'Tokyo','Japan', 'AMD001', 'B',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_012/PH_item_012_1.jpg?raw=true',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_012/PH_item_012_2.png?raw=true',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_012/PH_item_012_3.png?raw=true',
@@ -221,10 +222,10 @@ VALUES ('PH_item_012', 'Alien from Krr Planet House', 'T', 25000000.00,
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_012/PH_item_012_5.jpg?raw=true'
 );
 
-INSERT INTO Product (p_id, p_name, p_type, p_price, p_description, p_location, adminid, categories, picture1, picture2, picture3, picture4, picture5)
+INSERT INTO Product
 VALUES ('PH_item_013', 'Kame House', 'H', 1000000.00, 
         'The house is in the middle of nowhere in the sea.', 
-        'Nowhere, Pacific Ocean', 'AMD001', 'B',
+        'Okinawa', 'Japan', 'AMD001', 'B',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_013/PH_item_013_1.jpg?raw=true',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_013/PH_item_013_2.jpg?raw=true',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_013/PH_item_013_3.jpg?raw=true',
@@ -232,10 +233,10 @@ VALUES ('PH_item_013', 'Kame House', 'H', 1000000.00,
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_013/PH_item_013_5.jpg?raw=true'
 );
 
-INSERT INTO Product (p_id, p_name, p_type, p_price, p_description, p_location, adminid, categories, picture1, picture2, picture3, picture4, picture5)
+INSERT INTO Product
 VALUES ('PH_item_014', 'Tomoya\'s Condo', 'C', 500000.00, 
         'Nomal condo that has a very great background story inside.', 
-        'Osaka, Japan', 'AMD004', 'R',
+        'Osaka','Japan', 'AMD004', 'R',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_014/PH_item_014_1.png?raw=true',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_014/PH_item_014_2.png?raw=true',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_014/PH_item_014_3.jpg?raw=true',
@@ -243,10 +244,10 @@ VALUES ('PH_item_014', 'Tomoya\'s Condo', 'C', 500000.00,
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_014/PH_item_014_5.jpg?raw=true'
 );
 
-INSERT INTO Product (p_id, p_name, p_type, p_price, p_description, p_location, adminid, categories, picture1, picture2, picture3, picture4, picture5)
+INSERT INTO Product
 VALUES ('PH_item_015', 'Maria Townhouse', 'T', 150000.00, 
         'The townhouses that are inside the big wall Maria where the wall made of 50-meter titans.', 
-        'Shikanshina, Paradise', 'AMD003', 'R',
+        'Tokyo','Japan', 'AMD003', 'R',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_015/PH_item_015_1.png?raw=true',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_015/PH_item_015_2.png?raw=true',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_015/PH_item_015_3.jpg?raw=true',
@@ -254,10 +255,10 @@ VALUES ('PH_item_015', 'Maria Townhouse', 'T', 150000.00,
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_015/PH_item_015_5.jpg?raw=true'
 );
 
-INSERT INTO Product (p_id, p_name, p_type, p_price, p_description, p_location, adminid, categories, picture1, picture2, picture3, picture4, picture5)
+INSERT INTO Product
 VALUES ('PH_item_999', 'Haxxtxd House', 'HT', 2500.00, 
         'The house that has special history over than 130 years that many people believed it was the house that the famous serial killer in the late 18 century \'Jack the Ripper\' died inside.', 
-        'London, England', 'AMD004', 'R',
+        'London', 'England', 'AMD004', 'R',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_999/PH_item_999_1.jpg?raw=true',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_999/PH_item_999_2.jpg?raw=true',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_999/PH_item_999_3.jpg?raw=true',
