dc365255e4eb93f3aef8e6aeb773368c8dfb62d2
nuttapong-roc <109577863+nuttapong-roc@users.noreply.github.com>
2024-04-22 23:51:26 +0700

    Add files via upload

--- webproject.sql
+++ webproject.sql
@@ -5,30 +5,12 @@ USE `Pogohouse`;
 -- Table structure for table User
 --
 
-CREATE TABLE User (
-  user_id varchar(10) NOT NULL,
-  username varchar(20) NOT NULL,
-  user_fname varchar(20) NOT NULL,
-  user_lname varchar(20) NOT NULL,
-  age int  DEFAULT NULL,
-  Primary key (user_id)
-) ;
 
 --
 -- Dumping data for table User
 --
 
-INSERT INTO User (user_id, username, user_fname, user_lname) VALUES
-('US001', 'strawberryJam', 'Jamey', 'Oliver'),
-('US002', 'Nickko1234', 'Branch', 'Nicko'),
-('US003', 'MikeyBD', 'Mike', 'Brandon'),
-('US004', 'TestThisGave', 'Gavin', 'Testdee'),
-('US005', 'Timmy111', 'Tim', 'Hapor'),
-('US006', 'WendyBurger', 'Wendy', 'Welling'),
-('US007', 'LimeLieLiam', 'Liam', 'Bug'),
-('US008', 'QuincySoul', 'Qwinz', 'Hood'),
-('US009', 'GamingTime', 'Gaming', 'Wu'),
-('US010', 'Arasumsum', 'Arata', 'Nakamuya');
+
 
 -- --------------------------------------------------------
 create table Admin (
@@ -54,26 +36,25 @@ CREATE TABLE Account (
   contact varchar(10) NOT NULL,
   password varchar(20) NOT NULL,
   accountname varchar(20) NOT NULL,
-  userid varchar(10) NOT NULL,
-  adminid varchar(10) NOT NULL,
-  PRIMARY KEY (acc_id),
-  Foreign key (userid) references User(user_id),
-  Foreign key (adminid) references Admin(admin_id)
+  adminid varchar(10),
+  PRIMARY KEY (acc_id)
 ) ;
 
-
-
-INSERT INTO Account (acc_id, location, email, contact, password, accountname, userid, adminid) VALUES
-('PH101', 'Sydney', 'chucklechief@emailgiggle.com',	'0959549287', 'Serrage45', 'JorkerIRL', 'US001', 'AMD004'),
-('PH102', 'Bangkok', 'gsticky.rice@gmail.com', '0805649068', 'horrowprimedGreat77',	'LwnZa007',	'US002','AMD001'),
-('PH103', 'Rayong',	'kaosoy01@hotmail.com',	'0960263295', 'GaussTB1', 'BirdKonDee',	'US003', 'AMD002'),
-('PH104', 'West Virginia', 'williamafton87@gmail.com', '0956719509', 'FrBrowww0', 'BorrowMoney', 'US004', 'AMD003'),
-('PH105', 'Stockholm', 'markus.per@govmail.com', '0818082587', '123ASD888', 'Kevin2569', 'US005', 'AMD002'),
-('PH106', 'Corolado', 'wendywelling@hotmail.com',	'0850532088',	'12345678ABC',	'Balbazal90', 'US006',	'AMD004'),
-('PH107', 'Chiang Mai',	'lbug@gmail.com', '0947891446', 'X1Y2Z3wdym',	'Beam1999',	'US007', 'AMD002'),
-('PH108', 'California',	'misterheart.locker@gmail.com', '0949763489', 'pokemonPokemon10',	'DomtaoBoba', 'US008', 'AMD003'),
-('PH109', 'Hongkong', 'superidol199@baimail.com', '0830848207', 'SadSad99934', 'SuperIdol',	'US009', 'AMD002'),
-('PH110', 'Bangkok', 'teebindai@gmail.com',	'0863555858', 'SoulZadddd55', 'APoDragonW', 'US010', 'AMD004');
+INSERT INTO Account (acc_id, location, email, contact, password, accountname, adminid) VALUES
+('PH101', 'Sydney', 'chucklechief@emailgiggle.com',	'0959549287', 'Serrage45', 'JorkerIRL',null),
+('PH102', 'Bangkok', 'gsticky.rice@gmail.com', '0805649068', 'horrowprimedGreat77',	'LwnZa007',null),
+('PH103', 'Rayong',	'kaosoy01@hotmail.com',	'0960263295', 'GaussTB1', 'BirdKonDee',null),
+('PH104', 'West Virginia', 'williamafton87@gmail.com', '0956719509', 'FrBrowww0', 'BorrowMoney',null),
+('PH105', 'Stockholm', 'markus.per@govmail.com', '0818082587', '123ASD888', 'Kevin2569',null),
+('PH106', 'Corolado', 'wendywelling@hotmail.com',	'0850532088',	'12345678ABC',	'Balbazal90',null),
+('PH107', 'Chiang Mai',	'lbug@gmail.com', '0947891446', 'X1Y2Z3wdym',	'Beam1999', null),
+('PH108', 'California',	'misterheart.locker@gmail.com', '0949763489', 'pokemonPokemon10',	'DomtaoBoba',null),
+('PH109', 'Hongkong', 'superidol199@baimail.com', '0830848207', 'SadSad99934', 'SuperIdol',null),
+('PH110', 'Bangkok', 'teebindai@gmail.com',	'0863555858', 'SoulZadddd55', 'APoDragonW',null),
+('PH111', 'Bangkok', 'booktepbook@gmail.com','0954954034', '1234','booktepbook', 'AMD003'),
+('PH112', 'Bangkok', 'Attapong@gmail.com','0102030405', '1234','Dom', 'AMD004'),
+('PH113', 'Bangkok', 'Salmon@gmail.com','0302010504', '1234','Salmon', 'AMD001'),
+('PH114', 'Bangkok', 'Pawat@gmail.com','0708090405', '1234','Pame', 'AMD002');
 
 -- --------------------------------------------------------
 -- Table structure for table Transaction
@@ -266,31 +247,6 @@ VALUES ('PH_item_999', 'Haxxtxd House', 'HT', 2500.00,
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_999/PH_item_999_4.jpg?raw=true',
         'https://github.com/nuttapong-roc/pogohouse/blob/3dcc3da0e7524e27d79cd05fcfe594280e4965c1/house%20pic/PH_item_999/PH_item_999_5.jpg?raw=true'
 );
-
-        
-create table Transaction (
-    trans_id varchar(10),
-    trans_date date,
-    trans_time time,
-    amount decimal(13,2),
-    t_type char(1),
-    acc_id varchar(10),
-    p_id varchar(15),
-    foreign key  (acc_id) references Account(acc_id),
-    foreign key (p_id) references Product(p_id)
-);
-
-insert into Transaction (trans_id, trans_date, trans_time, amount, t_type, acc_id, p_id) 
-values	('HT73921', '2024-02-08', '07:42:18', 100000000.00, 'A', 'PH103', 'PH_item_001'),
-		('HT48203', '2024-03-02', '11:15:55', 3000000.00, 'C', 'PH102', 'PH_item_011'),
-		('HT61547', '2024-03-06', '14:53:21', 30000000.00, 'A', 'PH105', 'PH_item_007'),
-		('HT90482', '2024-04-03', '05:27:36', 400000.00, 'A', 'PH103', 'PH_item_005'),
-		('HT36715', '2024-04-14', '10:08:47', 25000000.00, 'C', 'PH101', 'PH_item_012'),
-		('HT52106', '2024-04-21', '15:36:12', 300000.00, 'C', 'PH108', 'PH_item_003'),
-		('HT87654', '2024-05-16', '09:19:28', 50000000.00, 'C', 'PH106', 'PH_item_009'),
-		('HT19328', '2024-05-11', '13:47:39', 300000.00, 'A', 'PH110', 'PH_item_004'),
-		('HT64092', '2024-05-28', '06:55:03', 150000.00, 'C', 'PH103', 'PH_item_015'),
-		('HT40875', '2024-06-25', '16:14:50', 3.00, 'C', 'PH104', 'PH_item_002');
         
 
         
